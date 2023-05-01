c64baf252aa3bd4f81ab58b0cd6eda5a815f0bd5
astuvan <astuvan@gmail.com>
2023-05-01 12:09:33 +0200

    Added title and working clock

--- animation/animation.js
+++ animation/animation.js
@@ -1,5 +1,14 @@
 function start() {
-    
+    // document.getElementById("txt_august").innerHTML = "";
+    // document.getElementById("txt_broch").innerHTML = "";
+
+    let clock = 0;
+
+    setInterval(animate, 100);
+
+    function animate() {
+        clock += 1;
+    }
 }
 
 window.onload = start;
